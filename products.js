36f9d61e74357e11aef4eeb3bfd2e65123657f22
Pradhumn Chandrawat <chandrawatpradhumn@gmail.com>
2024-01-15 22:50:50 +0530

    product cart

---
+++ products.js
@@ -0,0 +1,7 @@
+const productList = [
+  { id: 1, name: "vivo T2 5g", price: 29999 },
+  { id: 2, name: "Samsung S21 FE 5g", price: 29999 },
+  { id: 3, name: "Nothing Phone 2", price: 34999 },
+  { id: 4, name: "Moto Edge 40", price: 21999 },
+  { id: 5, name: "Realme 11 Pro 5g", price: 25999 },
+];
